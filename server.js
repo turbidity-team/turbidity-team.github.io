f5328592eb00980bc89ec110cdfecbae963c7f34
XIAOWEI WANG <xiaoweirwang@THE-NOODLER.local>
2019-04-28 10:01:46 -0700

    homepage

--- server.js
+++ server.js
@@ -2,18 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 const ee = require('@google/earthengine');
+const path = require('path');
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-const PRIVATE_KEY = require('./privatekey.json');
-const PORT = process.env.PORT || 3000;
-
-ee.data.authenticateViaPrivateKey(PRIVATE_KEY, () => {
-  ee.initialize(null, null, () => {
-    app.listen(PORT);
-    console.log(`Listening on port ${PORT}`);
-  });
-});
+// const PRIVATE_KEY = require('./privatekey.json');
+// const PORT = process.env.PORT || 3000;
+//
+// ee.data.authenticateViaPrivateKey(PRIVATE_KEY, () => {
+//   ee.initialize(null, null, () => {
+//     app.listen(PORT);
+//     console.log(`Listening on port ${PORT}`);
+//   });
+// });
